ff6bcda9e58f2cbb6495f97109cdaa84eb7c151b
unknown <eric.bryant0@gmail.com>
2020-08-06 19:16:30 -0400

    setup api calls to backend

---
+++ src/services/apiCalls.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+  baseURL: `http://localhost:3000`,
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
+})
+
+export default {
+  searchMulti(query) {
+    return apiClient.get(`/search/multi/${query}`)
+  },
+  searchPerson(query) {
+    return apiClient.get(`/search/person/${query}`)
+  },
+  searchMovie(query) {
+    return apiClient.get(`/search/movie/${query}`)
+  },
+  searchTV(query) {
+    return apiClient.get(`/search/tv/${query}`)
+  },
+  getMovieDetails(movieID) {
+    return apiClient.get(`/movie/${movieID}`)
+  }
+}
