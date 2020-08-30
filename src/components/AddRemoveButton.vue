51ed577ecdd8a74730a48ac47d42f73a0f398201
unknown <eric.bryant0@gmail.com>
2020-08-30 13:18:41 -0400

    add / remove from your list of choice

--- src/components/AddRemoveButton.vue
+++ src/components/AddRemoveButton.vue
@@ -1,9 +1,36 @@
 <template>
   <div v-if="onList">
-    <v-btn @click="removeFromWatchList" color="error">Remove</v-btn>
-    <v-btn @click="addToWatched" color="accent" class="ml-2">Watched</v-btn>
+    <v-menu offset-y>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn color="error" v-bind="attrs" v-on="on">Remove</v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="(list, index) in onTheseLists"
+          :key="index"
+          @click="removeFromList(list.name)"
+        >
+          <v-list-item-title>{{ list.name }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
   </div>
-  <v-btn v-else @click="addToWatchList" color="accent">Add To Watch List</v-btn>
+  <div v-else>
+    <v-menu offset-y>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn color="accent" v-bind="attrs" v-on="on">Add</v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="(list, index) in lists"
+          :key="index"
+          @click="addToList(list.name)"
+        >
+          <v-list-item-title>{{ list.name }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </div>
 </template>
 
 <script>
@@ -23,7 +50,8 @@ export default {
   },
   data() {
     return {
-      onList: false
+      onList: false,
+      lists: []
     }
   },
   watch: {
@@ -34,11 +62,36 @@ export default {
     }
   },
   computed: {
-    ...mapGetters(['getUID', 'getLists'])
+    ...mapGetters(['getUID', 'getLists']),
+    onTheseLists() {
+      return this.lists.filter(list => {
+        return list.isOnThisList === true
+      })
+    }
   },
   methods: {
     addToWatchList() {
       dbClient.addMediaToWatchList(this.getUID, this.mediaInfo)
+    },
+    addToList(listName) {
+      let fieldName
+      for (let key in this.getLists) {
+        if (listName === this.getLists[key].name) {
+          fieldName = key
+        }
+      }
+
+      dbClient.addToList(this.getUID, this.mediaInfo, fieldName)
+    },
+    removeFromList(listName) {
+      let fieldName
+      for (let key in this.getLists) {
+        if (listName === this.getLists[key].name) {
+          fieldName = key
+        }
+      }
+
+      dbClient.removeFromList(this.getUID, this.mediaInfo.id, fieldName)
     },
     removeFromWatchList() {
       dbClient.removeMediaFromWatchList(this.getUID, this.mediaInfo.id)
@@ -47,16 +100,17 @@ export default {
       dbClient.addToWatchedList(this.getUID, this.mediaInfo)
     },
     checkIfOnList() {
-      const lists = []
+      this.lists = []
       for (let key in this.getLists) {
-        lists.push(this.getLists[key])
+        this.lists.push(this.getLists[key])
       }
 
       this.onList = false
 
-      lists.map(list => {
+      this.lists.map(list => {
         if (list.onList[this.mediaInfo.id]) {
           this.onList = true
+          list.isOnThisList = true
         }
       })
     }
--- src/services/dbCalls.js
+++ src/services/dbCalls.js
@@ -25,6 +25,18 @@ export default {
         refDoc.update(watchedListUpdate)
       })
   },
+  addToList(userID, media, listField) {
+    const refDoc = Firebase.db.collection('lists').doc(userID)
+    const listUpdate = {}
+    listUpdate[`${listField}.onList.${media.id}`] = media
+    refDoc.update(listUpdate)
+  },
+  removeFromList(userID, mediaID, listField) {
+    const refDoc = Firebase.db.collection('lists').doc(userID)
+    refDoc.update({
+      [`${listField}.onList.${mediaID}`]: Firebase.dbDelete.delete()
+    })
+  },
   async getUsersLists(userID) {
     const refDoc = Firebase.db.collection('lists').doc(userID)
     const userData = await refDoc.get()
